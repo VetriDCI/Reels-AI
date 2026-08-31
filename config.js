99eb6c67928d32ce371ce187a6d28d88a5f3bbb0
VetriDCI <vetri.dci23@gmail.com>
2026-08-31 17:58:17 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// RA Social Supabase public browser configuration
-// Do NOT add a secret key or service_role key here.
-export const SUPABASE_URL = 'https://ojmlfcyftdydkzqkbnhs.supabase.co';
-
-export const SUPABASE_PUBLISHABLE_KEY =
-  'sb_publishable_IuRhlHhmVxCxhPcEFlGeIQ_3bI2UfOM';
