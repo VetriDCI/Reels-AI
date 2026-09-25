bcdf54d8a530430340b1cf86d53133fa05f449ea
VetriDCI <vetri.dci23@gmail.com>
2026-09-25 18:11:41 +0530

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { protect } from '../middleware/authMiddleware.js';
-import { getPublicUser } from '../controllers/userController.js';
-const router = express.Router();
-router.get('/:id', protect, getPublicUser);
-export default router;
