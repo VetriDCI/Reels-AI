fa3c1d5ff1eb88f053d1da510106024b3245c7fe
VetriDCI <vetri.dci23@gmail.com>
2026-09-25 18:21:33 +0530

    Delete reportRoutes.js

--- reportRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { protect } from '../middleware/authMiddleware.js';
-import { getMyReports } from '../controllers/reportController.js';
-
-const router = express.Router();
-router.get('/mine', protect, getMyReports);
-export default router;
