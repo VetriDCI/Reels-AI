763e38c634c77f1530d63451146f1d00ab05ecef
VetriDCI <vetri.dci23@gmail.com>
2026-09-26 08:32:25 +0530

    Delete adRoutes.js

--- adRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { listActiveAds, recordAdEvent } from '../controllers/adController.js';
-import { protect } from '../middleware/authMiddleware.js';
-const router = express.Router();
-router.get('/', listActiveAds);
-router.post('/:id/event', protect, recordAdEvent);
-export default router;
